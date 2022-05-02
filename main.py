f36eb631f4c09c35d8ae0ea4ac1f95b9e236d5e6
mbardoe <mbardoe@gmail.com>
2022-05-02 11:04:06 -0400

    Took out track_width

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 from pose2d import Pose2D
 
 
-def SwerveCommand(origin: Pose2D, target: Pose2D, max_linear_speed, max_angular_speed, track_width, speed):
+def SwerveCommand(origin: Pose2D, target: Pose2D, max_linear_speed, max_angular_speed, speed):
     """"SwerveCommmand takes in 2 poses and returns a tuple of time, and 4 directions for the 4 wheels
     of a swerve drive robot. The speed is a percentage of the max speed to allow. """
     delta_x = target.x - origin.x
